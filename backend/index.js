9e61d627e20dfebc7b9924d1ca8774ff657dcdb2
tsomani <tabreek.somani@gmail.com>
2022-10-15 13:10:28 -0700

    Attempt to create database connection

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,33 @@
 import express from 'express'
+import * as dotenv from 'dotenv'
+dotenv.config()
+import mysql from 'mysql'
+
 
 const app = express()
 const port = 3000
 
+
+const connection = mysql.createConnection({
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT
+});
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+  connection.connect(function(err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+    }
+
+    console.log('Connected to database.');
+  });
+  connection.end();
 })
 
 app.listen(port, () => {
