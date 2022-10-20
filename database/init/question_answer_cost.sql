8649bee72a0a00137e29904f040a179d0d229298
Martin Tang <martintang8422@gmail.com>
2022-10-19 19:46:08 -0700

    Question_Cost

--- database/init/question_answer_cost.sql
+++ database/init/question_answer_cost.sql
@@ -11,12 +11,14 @@ BEGIN
 
 CREATE TABLE `questions_cost` (
 	cost_id VARCHAR(50),
-	fk_answer_id VARCHAR(50),
+	fk_question_id VARCHAR(50),
 	cost DOUBLE,
 	PRIMARY KEY (`cost_id`),
-	FOREIGN KEY (fk_answer_id) REFERENCES questions_answer(answer_id)
+	FOREIGN KEY (fk_question_id) REFERENCES questions(question_id)
 );
 
 END$$
 
 DELIMITER ;
+
+19:37:04	CALL createQuestions_cost()	Error Code: 1072. Key column 'fk_answer_id' doesn't exist in table	0.059 sec
