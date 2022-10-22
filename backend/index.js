5505046691c94d9fdd1f04cd3c5a705cd4e907e4
tsomani <tabreek.somani@gmail.com>
2022-10-22 10:33:20 -0700

    Database Connection Refactored

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,21 @@ import express from 'express'
 import bodyParser from 'body-parser';
 import questionRouter from './src/routes/questionRoute.js'
 import {Database} from "./src/config/Database.js";
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 
 const app = express()
 const port = 8080
-const db = new Database();
+const config = {
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DB,
+  port: process.env.RDS_PORT
+}
+const db = new Database(config);
+db.connect();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,9 +25,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (_, res) => {
   res.send('Hello Labby!')
-  if (!db.isConnected()) {
-    db.connect();
-  }
 
 })
 
--- backend/src/config/Database.js
+++ backend/src/config/Database.js
@@ -1,33 +1,25 @@
 import mysql from "mysql";
-import connection from "mysql/lib/Connection.js";
-import * as dotenv from 'dotenv'
-dotenv.config({path:"../../env"})
+
 export class Database {
 
 
     connection;
+    constructor(config) {
+        this.connection = mysql.createConnection(config);
+    }
 
     connect() {
-        this.connection = mysql.createConnection({
-            host: process.env.RDS_HOSTNAME,
-            user: process.env.RDS_USERNAME,
-            password: process.env.RDS_PASSWORD,
-            port: process.env.RDS_PORT
-        });
-        this.connection.connect(function(err) {
-            if (err) {
-                console.log(connection.port);
-                console.error('Database.js connection failed: ' + err.stack);
-                return;
-            }
+            this.connection.connect(function (err) {
+                if (err) {
+                    console.error('Database.js connection failed: ' + err.stack);
+                    return;
+                }
 
-            console.log('Connected to database.');
-        });
+                console.log('Connected to database.');
+            });
     }
 
-    isConnected() {
-        return this.connection.isConnected();
-    }
+
 
     close() {
         this.connection.end();
