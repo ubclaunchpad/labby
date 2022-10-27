12466e1ef028de57579b6b6939ef8a616f4e7114
mathew bushuru <bushuruwafulamathew@gmail.com>
2022-10-27 12:56:52 -0700

    add structure of components to sideview

--- frontend/src/components/BuilderLibrary/ComponentLibrary/index.js
+++ frontend/src/components/BuilderLibrary/ComponentLibrary/index.js
@@ -1,10 +1,31 @@
 import "./index.css";
+import { DefaultCard } from "../../Card";
 
 function ComponentLibrary() {
   return (
-    <div>
-     ComponentLibrary
-    </div>
+    <>
+      <div className="elementContainer">
+        <div className="elementsTitle">Question Elements</div>
+        <DefaultCard>Multiple choice</DefaultCard>
+        <DefaultCard> Single selection </DefaultCard>
+        <DefaultCard> Text answer</DefaultCard>
+        <DefaultCard> Dropdown </DefaultCard>
+      </div>
+      <div className="elementContainer">
+        <div className="elementsTitle">Layout Elements</div>
+        <DefaultCard> Heading </DefaultCard>
+        <DefaultCard> Text line </DefaultCard>
+      </div>
+      <div className="elementContainer">
+        <div className="elementsTitle">Media Elements</div>
+        <DefaultCard> File upload </DefaultCard>
+        <DefaultCard> File download </DefaultCard>
+      </div>
+      <div className="elementContainer">
+        <div className="elementsTitle">Billing Elements</div>
+        <DefaultCard> Contact information </DefaultCard>
+      </div>
+    </>
   );
 }
 
