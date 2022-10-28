a93a8fcd652f86cefbf03f4b5f637250f11fa095
Hao (Harin) Wu <46661377+Harin329@users.noreply.github.com>
2022-10-28 14:51:18 -0700

    Loads Existing Questions (#23)

--- database/dummy/workflow1.sql
+++ database/dummy/workflow1.sql
@@ -2,3 +2,10 @@
 
 -- Populate the form title
 CALL save_question('RANDOM-ID-A','My Sample Form','heading', 0);
+
+-- Populate some sample questions
+CALL save_question('RANDOM-ID-B','Request Type','multi', 1);
+CALL save_question('RANDOM-ID-C','Contact Details','contact', 2);
+CALL save_question('RANDOM-ID-D','Study Title','textline', 3);
+CALL save_question('RANDOM-ID-E','Sample Dropdown','dropdown', 4);
+CALL save_question('RANDOM-ID-F','Sample Upload','upload', 5);
--- frontend/src/components/FormBuilder/index.js
+++ frontend/src/components/FormBuilder/index.js
@@ -1,8 +1,110 @@
+import { useSelector } from "react-redux";
 import { appColor } from "../../constants";
 import FormTitle from "./FormTitle";
 import "./index.css";
 
 function FormBuilder() {
+  const questionList = useSelector(
+    (state) => state.questionReducer.questionList
+  );
+
+  function renderQuestion(question) {
+    switch (question.question_type) {
+      case "multi":
+        return (
+          <div
+            className="FormBuilderQuestion"
+            key={question.question_id}
+            style={{ color: appColor.gray }}
+          >
+            {question.question + " @ Q" + question.position_index}
+          </div>
+        );
+      case "single":
+        return (
+          <div
+            className="FormBuilderQuestion"
+            key={question.question_id}
+            style={{ color: appColor.gray }}
+          >
+            {question.question + " @ Q" + question.position_index}
+          </div>
+        );
+      case "text":
+        return (
+          <div
+            className="FormBuilderQuestion"
+            key={question.question_id}
+            style={{ color: appColor.gray }}
+          >
+            {question.question + " @ Q" + question.position_index}
+          </div>
+        );
+      case "dropdown":
+        return (
+          <div
+            className="FormBuilderQuestion"
+            key={question.question_id}
+            style={{ color: appColor.gray }}
+          >
+            {question.question + " @ Q" + question.position_index}
+          </div>
+        );
+      case "heading":
+        return (
+          <div
+            className="FormBuilderQuestion"
+            key={question.question_id}
+            style={{ color: appColor.gray }}
+          >
+            {question.question + " @ Q" + question.position_index}
+          </div>
+        );
+      case "textline":
+        return (
+          <div
+            className="FormBuilderQuestion"
+            key={question.question_id}
+            style={{ color: appColor.gray }}
+          >
+            {question.question + " @ Q" + question.position_index}
+          </div>
+        );
+      case "upload":
+        return (
+          <div
+            className="FormBuilderQuestion"
+            key={question.question_id}
+            style={{ color: appColor.gray }}
+          >
+            {question.question + " @ Q" + question.position_index}
+          </div>
+        );
+      case "download":
+        return (
+          <div
+            className="FormBuilderQuestion"
+            key={question.question_id}
+            style={{ color: appColor.gray }}
+          >
+            {question.question + " @ Q" + question.position_index}
+          </div>
+        );
+      case "contact":
+        return (
+          <div
+            className="FormBuilderQuestion"
+            key={question.question_id}
+            style={{ color: appColor.gray }}
+          >
+            {question.question + " @ Q" + question.position_index}
+          </div>
+        );
+      default:
+        return null;
+    }
+  }
+
   return (
     <div>
       <div className="FormBuilderHeader FormBuilder">
@@ -36,8 +138,14 @@ function FormBuilder() {
         </div>
       </div>
       <div className="ScrollBox FormBuilder">
-        <div className="FormBuilderOutline" style={{ color: appColor.gray }}>
-          Drag and drop to add components
+        <div className="FormBuilderOutline">
+          {questionList.length ? (
+            questionList.slice(1).map((question) => renderQuestion(question))
+          ) : (
+            <div className="DragAndDropText" style={{ color: appColor.gray }}>
+              Drag and drop to add components
+            </div>
+          )}
         </div>
       </div>
     </div>
