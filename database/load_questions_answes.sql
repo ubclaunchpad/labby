bc1cc772d648e51f7fd7756afa3e415bee4a5b3f
jjessieshang <17jessies@gmail.com>
2022-10-28 13:40:24 -0700

    procedure name changes

--- database/load_questions_answes.sql
+++ database/load_questions_answes.sql
@@ -1,9 +1,9 @@
 USE `labby.db`;
-DROP procedure IF EXISTS `join_questions_answers`;
+DROP procedure IF EXISTS `load_questions_answers`;
 
 DELIMITER $$
  
-CREATE PROCEDURE `join_questions_answers` ()
+CREATE PROCEDURE `load_questions_answers` ()
 
 BEGIN
     SELECT question, questions.question_type, answer
