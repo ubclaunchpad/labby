24cf34bad2aab2a45948056ab26803e81bd09c9e
Martin Tang <62964506+TangMartin@users.noreply.github.com>
2022-10-30 09:58:49 -0700

    BE Feature -  Create and Delete Answer Endpoint (#25)
    * Answer Route Created
    
    * Create and Delete Answer Endpoint
    
    * Remove .history
    
    Co-authored-by: HarinHaoWu <master.harin@gmail.com>

---
+++ backend/src/routes/answerRoute.js
@@ -0,0 +1,36 @@
+import { Router } from "express";
+import AnswerController from "../controllers/answerController.js";
+
+const router = Router();
+const answerController = new AnswerController();
+
+router.post("/", (req, res) => {
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!",
+    });
+    return;
+  }
+  answerController
+    .saveAnswer(req)
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      res.status(404).json(err);
+    });
+});
+
+
+router.delete("/", (req, res) => {
+  answerController
+    .deleteAnswer(req)
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      res.status(404).json(err);
+    });
+});
+
+export default router;
