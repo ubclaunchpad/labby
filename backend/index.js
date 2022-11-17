c307e11e498fd9e6faeec52f85bc1718a928d058
Martin Tang <62964506+TangMartin@users.noreply.github.com>
2022-11-17 10:04:52 -0800

    BE - Survey, Response Endpoint (#43)
    * Response Route, Controller, Model
    
    * Save Response Endpoint
    
    * POST Survey, Response, DELETE Survey
    
    * Bug Fixes
    
    * Added On-Delete Cascade for Survey

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import questionRouter from "./src/routes/questionRoute.js";
 import logicRouter from "./src/routes/logicRoute.js";
 import answerRouter from "./src/routes/answerRoute.js";
 import orderRouter from "./src/routes/orderRoute.js";
+import surveyRouter from "./src/routes/surveyRoute.js"
 
 const app = express();
 const port = 8080;
@@ -21,6 +22,7 @@ app.use("/question", questionRouter);
 app.use("/logic", logicRouter);
 app.use("/answer", answerRouter);
 app.use("/reorder", orderRouter);
+app.use("/survey", surveyRouter);
 
 app.listen(port, () => {
   console.log(`Labby backend listening on port ${port}`);
