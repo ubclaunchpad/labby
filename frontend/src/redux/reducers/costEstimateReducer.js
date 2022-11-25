f1424eb8f0a26f9319eed803dbd44f320cfcd6ef
jjessieshang <17jessies@gmail.com>
2022-11-25 13:05:50 -0800

    1st commit

--- frontend/src/redux/reducers/costEstimateReducer.js
+++ frontend/src/redux/reducers/costEstimateReducer.js
@@ -2,8 +2,20 @@ import { combineReducers } from "redux";
 import { TOGGLE_LOGIC } from "../actions/uiActions";
 
 const defaultCostEstimate = true;
+const defaultCostEstimateList = [];
 
 const costEstimateView = (state = defaultCostEstimate, action) => {
+  switch (action.type) {
+    case TOGGLE_LOGIC: {
+      return !state;
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+const questionList = (state = defaultQuestionList, action) => {
   switch (action.type) {
     case TOGGLE_LOGIC: {
       return !state;
