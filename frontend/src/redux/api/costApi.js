94410b1795d42c40c4a27c4c18d71d837602baed
jjessieshang <17jessies@gmail.com>
2022-11-24 16:05:37 -0800

    commit1

--- frontend/src/redux/api/costApi.js
+++ frontend/src/redux/api/costApi.js
@@ -1,1 +1,17 @@
-//Get services from backend here
+import defaultAxios from "axios";
+import { backend } from "../../constants";
+
+const axios = defaultAxios.create({
+  baseURL: backend,
+  headers: { "Content-Type": "application/json" },
+});
+
+//cost estimate with backend
+export const getCostEstimate = async () => {
+    try {
+      const costEstimates = await axios.get(“question/“);
+      return costEstimates;
+    } catch (err) {
+      return console.error(err);
+    }
+  };
