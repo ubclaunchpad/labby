496ebe35069c40493c3b4d2424ae08da0e503081
Arman Moztarzadeh <DEMONSLAYER62@Armans-MacBook-Air.local>
2023-01-07 14:53:12 -0800

    add load and save billable routes

--- backend/src/models/billable.js
+++ backend/src/models/billable.js
@@ -11,4 +11,26 @@ export class Billable {
       }
     });
   }
+
+  deleteBillable(billableId, result) {
+    con.query("CALL delete_billable(?)", billableId, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+      } else {
+        result(null, res[0]);
+      }
+    });
+  }
+
+  saveBillable(billable, result) {
+    con.query("CALL save_billable(?,?,?,?,?,?,?,?,?)", billable, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+      } else {
+        result(null, res[0]);
+      }
+    });
+  }
 }
