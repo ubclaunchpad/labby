f4240f550648f4c9b5f42fa02d9e2ff6c1bfb4da
Sue Lee <suuelee@outlook.com>
2023-01-12 23:16:53 -0800

    creates cost centre endpoints

---
+++ backend/src/controllers/costCentreController.js
@@ -0,0 +1,46 @@
+import { CostCentre, CostCentre} from "../models/costCentre.js";
+
+export default class CostCentre {
+    saveCostCentre(req) {
+        return new Promise((resolve, reject) => {
+            const CostCentreMode = new CostCentre();
+
+            const costCentre = {
+                costCentre_id: req.body.costCentre_id, // add any others here
+            };
+
+            CostCentreModel.insertCostCentre(costCentre, (err, result) => {
+                if (err) {
+                    reject({ error: err});
+                }
+                resolve(result);
+            });
+        });
+    }
+
+    deleteCostCentre(id) { // note: this also needs to be deleted from the organization
+        return new Promise((resolve, reject) => {
+            const CostCentreModel = new CostCentre();
+
+            CostCentreModel.deleteCostCentre(id, (err, result) => {
+                if (err) {
+                    reject( {error: err });
+                }
+                resolve(result);
+            });
+        });
+    }
+
+    assignCostCentre(organizationId) {
+        return new Promise((resolve, reject) => {
+            const CostCentreModel = new CostCentre();
+
+            CostCentreModel.assignCostCentre(organizationId, (err, result) => {
+                if (err) {
+                    reject( { error, err });
+                }
+                resolve(result);
+            })
+        });
+    }
+}
