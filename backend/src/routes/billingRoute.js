cb1aec5f51c805e61f2894ee1133a5e87d4ecfa6
Arman Moztarzadeh <armanmoztar@outlook.com>
2023-01-13 17:39:44 -0800

    add load and save billable routes (#57)
    * add load and save billable routes
    
    * Fix billable backend
    
    Co-authored-by: Arman Moztarzadeh <DEMONSLAYER62@Armans-MacBook-Air.local>
    Co-authored-by: HarinHaoWu <master.harin@gmail.com>

--- backend/src/routes/billingRoute.js
+++ backend/src/routes/billingRoute.js
@@ -15,4 +15,26 @@ router.get("/", (_, res) => {
       });
   });
 
+router.post("/", (req, res) => {
+    billingController
+      .saveBillable(req)
+      .then((response) => {
+        res.status(200).json(response);
+      })
+      .catch((err) => {
+        res.status(404).json(err);
+      });
+  });
+
+router.delete("/:id", (req, res) => {
+    billingController
+      .deleteBillable(req.params.id)
+      .then((response) => {
+        res.status(200).json(response);
+      })
+      .catch((err) => {
+        res.status(404).json(err);
+      });
+  });
+
 export default router;
