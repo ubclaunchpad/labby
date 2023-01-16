1cacb9e0863264757e393c7fafc23ef7e2adbc08
HarinHaoWu <master.harin@gmail.com>
2023-01-15 22:51:14 -0800

    remove empty line

--- backend/src/routes/costCentreRoute.js
+++ backend/src/routes/costCentreRoute.js
@@ -8,7 +8,6 @@ router.post("/", (req, res) => {
     if (!req.body) {
         res.status(400).send({
             message: "Content cannot be empty!",
-        
         });
         return;
     }
