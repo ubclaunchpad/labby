f2053131a1a9702d046287b8f12fa010234c2365
tabreeksomani <53943583+tabreeksomani@users.noreply.github.com>
2023-01-15 22:51:42 -0800

    Cost management backend (#60)
    * creates cost centre endpoints
    
    * fixes typos and adds comments
    
    * remove empty line
    
    Co-authored-by: Sue Lee <suuelee@outlook.com>

---
+++ backend/src/models/costCentre.js
@@ -0,0 +1,54 @@
+import con from "../config/Database.js";
+
+export class CostCentre {
+    insertCostCentre(newCostCentre, result) {
+        con.query(
+            "CALL save_cost_centre(?)",  // stored procedure doesn't exist yet
+            [
+                newCostCentre.costCentre_id, 
+            ], 
+            function (error, results) {
+                if (error) {
+                    console.log("error: ", error);
+                    result(error, null);
+                } else {
+                    result(null, {
+                        result: `Response ${results.costCentre_id} Saved Successfully`,
+                    });
+                }
+            }
+        );
+    }
+
+    assignCostCentre(organizationId, costCentreId, result) {
+        con.query(
+            "CALL assign_organization_cost_centre(?, ?)", // stored procedure should add costCentreId to the organization's table
+            [
+                organizationId, 
+                costCentreId,
+            ], 
+            function(error, results) {
+                if (error) {
+                    console.log("error: ", error);
+                    results(error, null);
+                } else {
+                    result(null, {
+                        result: `Organization ${organizationId} Sucessfully Assigned to Cost Centre ${costCentreId}`,
+                    });
+                }
+            }
+        );
+    }
+    deleteCostCentre(id, result) {
+        con.query(`CALL delete_cost_centre(?)`, [id], (err, res) => { // have to delete the cost centre from the organization too
+            if (err) {
+                console.log("error: ", err);
+                result(err, null);
+            } else {
+                result(null, res);
+            }
+        });
+    }
+
+}
+
---
+++ backend/src/routes/costCentreRoute.js
@@ -0,0 +1,48 @@
+import { response, Router } from "express";
+import CostCentreController from "../controllers/costCentreController";
+
+const router = Router();
+const costCentreController = new CostCentreController();
+
+router.post("/", (req, res) => {
+    if (!req.body) {
+        res.status(400).send({
+            message: "Content cannot be empty!",
+        });
+        return;
+    }
+    costCentreController.saveCostCentre(req)
+    .then((response) => {
+        res.status(200).json(response);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(404).json(err);
+    });
+})
+
+router.post("/:organizationId/:costCentreId", (req, res) => {
+    costCentreController
+    .assignCostCentre(req.params.organizationId, req.params.costCentreId)
+    .then((response) => {
+        res.status(200).json(response);
+    })
+    .catch((err) => {
+        res.status(404).json(err);
+    });
+});
+
+router.delete("/:costCentreId", (req, res) => {
+    costCentreController
+    .deleteCostCentre(req.params.costCentreId)
+    .then((response) => {
+        res.status(200).json(response);
+    })
+    .catch((err) => {
+        res.status(404).json(err);
+    });
+
+});
+
+export default router;
+
