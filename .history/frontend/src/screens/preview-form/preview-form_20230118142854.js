df373fd62a3421cfdb99972fd51525df6ae3fd19
Martin Tang <martintang8422@gmail.com>
2023-01-18 14:44:29 -0800

    Frontend Preview Form Complete

---
+++ .history/frontend/src/screens/preview-form/preview-form_20230118142854.js
@@ -0,0 +1,38 @@
+
+
+import { appColor } from "../../constants";
+import Header from "../../components/Header";
+import "./invoice.css";
+import InvoiceTable from "../../components/InvoiceTable";
+import { useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { LOAD_BILLABLE } from "../../redux/actions/billingActions";
+import GenerateInvoice from "../../components/GenerateInvoice";
+
+function Preview() {
+  const dispatch = useDispatch();
+  const invoiceTableRef = useRef(null);
+
+  useEffect(() => {
+    dispatch({ type: LOAD_BILLABLE });
+  }, [dispatch]);
+
+  return (
+    <div className="invoicePage">
+      <div className="headerComponent">
+        <Header />
+      </div>
+      <div className="invoicePageContent">
+        <div className="InvoiceTitle" style={{ color: appColor.gray }}>
+          Invoice Dashboard
+        </div>
+        <div className="InvoiceTable" ref={invoiceTableRef}>
+          <InvoiceTable />
+        </div>
+        <GenerateInvoice />
+      </div>
+    </div>
+  );
+}
+
+export default Preview;
