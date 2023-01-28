4993dcb5c3d47058f86baae09a897fa1e2f6a8a6
Athens <44182775+Athenstan@users.noreply.github.com>
2023-01-28 10:26:10 -0800

    Changed alerts into toasts that have animations and fold away (#65)
    Co-authored-by: Hao (Harin) Wu <46661377+Harin329@users.noreply.github.com>

---
+++ frontend/src/components/Toasts/index.js
@@ -0,0 +1,28 @@
+import clsx from "clsx";
+import { toast } from "react-toastify";
+
+import "./index.css";
+
+export function SuccessToast(message) {
+  return toast.success(message, {
+    className: "toast-notification",
+    autoClose: 2000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: false,
+    progress: false,
+  });
+}
+
+export function WarningToast(message) {
+  return toast.warning(message, {
+    className: "toast-notification",
+    autoClose: 2000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: false,
+    progress: false,
+  });
+}
