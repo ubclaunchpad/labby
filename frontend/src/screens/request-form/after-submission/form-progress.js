441e5504a6eda4f05f634572c9684721573c942a
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-03-09 02:02:46 -0800

    update progress page

--- frontend/src/screens/request-form/after-submission/form-progress.js
+++ frontend/src/screens/request-form/after-submission/form-progress.js
@@ -1,21 +1,43 @@
 import React from "react";
 
+import FormImg from "../../../assets/Form.png";
+
 import "./form-progress.css";
 
 function FormProgress() {
   return (
     <div className="formProgressPage">
-      <h2>Your requested services</h2>
-      <ProgressItem
-        code="SOW123"
-        progress={25}
-        comments="Lorem ipsum dolor sit amet, et paulo voluptatum pro, erat delenit posidonium est in. Ut iisque ornatus eam, ei "
-      />
-      <ProgressItem code="SOW121" progress={50} />
+      <div className="signout">
+        <button className="signoutBtn" type="button">
+          Sign out
+        </button>
+      </div>
+      <h2 className="pageTitle">MAPcore Services</h2>
 
-      <hr />
-      <h4>Completed Requests</h4>
-      <ProgressItem code="SOW115" progress={100} />
+      <div className="wrapper">
+        <div className="newServices">
+          <h3>Request a new service</h3>
+          <div className="newServicesWrapper">
+            <NewService serviceForm="MAPCore Service Request Form" />
+            <NewService serviceForm="MAPCore Clinical Service Request Form" />
+            <NewService serviceForm="MAPCore Consultation Request Form" />
+          </div>
+        </div>
+
+        <div className="requestedServices">
+          <h3>Your requested services</h3>
+          <ProgressItem
+            code="SOW123"
+            progress={25}
+            comments="Lorem ipsum dolor sit amet, et paulo voluptatum pro, erat delenit posidonium est in. Ut iisque ornatus eam, ei "
+          />
+          <ProgressItem code="SOW121" progress={50} />
+
+          <hr />
+          <h4>Completed Requests</h4>
+          <ProgressItem code="SOW115" progress={100} />
+        </div>
+      </div>
     </div>
   );
 }
@@ -35,6 +57,15 @@ function ProgressBar({ completed }) {
   return (
     <div className="progressBar">
       0% <progress max={100} value={completed}>{`${completed}%`}</progress> 100%
+    </div>
+  );
+}
+
+function NewService({ serviceForm }) {
+  return (
+    <div className="newService">
+      <img src={FormImg} alt="formIcon" className="formIcon" />
+      {serviceForm}
     </div>
   );
 }
