eea0a26afc7bfc8e19956b14b748343f095e5511
Hao (Harin) Wu <46661377+Harin329@users.noreply.github.com>
2023-03-25 18:13:20 -0700

    Revert "Multiple Text Answers - PR (#98)"
    This reverts commit bbeb90ab9ba3026e5cbfdcbb77df53f32d61672c.

--- frontend/src/redux/reducers/formReducer.js
+++ frontend/src/redux/reducers/formReducer.js
@@ -63,10 +63,10 @@ const formResponses = (state = defaultAnswerList, action) => {
   switch (action.type) {
     case ADD_RESPONSE: {
       state = state.filter(
-        (response) => (
+        (response) =>
           response.question.question_id !==
             action.payload.question.question_id ||
-          response.response !== action.payload.response)
+          response.response !== action.payload.response
       );
       state.push(action.payload);
       return [...state];
@@ -220,7 +220,7 @@ const formSubmissions = (state = defaultFormSubmissions, action) => {
   switch (action.type) {
     case SUBMIT_FORM: {
       state.push(action.payload);
-      // defaultAnswerList = [];
+      //defaultAnswerList = [];
       // TODO: save submitted forms to backend
       return state;
     }
