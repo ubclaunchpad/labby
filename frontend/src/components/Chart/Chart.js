f272a3952c948ce97c0cc1465818a8c443557568
Harin <master.harin@gmail.com>
2023-05-15 18:57:02 -0700

    Billing Invoice Styling and Graph Impl

--- frontend/src/components/Chart/Chart.js
+++ frontend/src/components/Chart/Chart.js
@@ -8,28 +8,42 @@ import {
   ResponsiveContainer,
 } from "recharts";
 import "./Chart.css";
+import { useSelector } from "react-redux";
 
 export const Chart = ({ data }) => {
-  let fakeData = [
-    { name: "Jun", Total: 10 },
-    { name: "Jul", Total: 21 },
-    { name: "Aug", Total: 7 },
-    { name: "Sep", Total: 14 },
-    { name: "Oct", Total: 9 },
-    { name: "Nov", Total: 19 },
-    { name: "Dec", Total: 9 },
-    { name: "Jan", Total: 1 },
-  ];
+  const invoiceDataSourceOG = useSelector(
+    (state) => state.billingReducer.billingListOG
+  );
+
+  let billingMapping = {};
+
+  invoiceDataSourceOG.forEach((invoice) => {
+    const invoiceDate = invoice.createdDate;
+    if (invoiceDate) {
+      const date = new Date(invoiceDate);
+      const month = date.getMonth();
+      const year = date.getFullYear();
+      const monthName = new Intl.DateTimeFormat("en-US", {
+        month: "short",
+      }).format(date);
+      const monthID = monthName + " " + year;
+
+      if (billingMapping[monthID]) {
+        billingMapping[monthID].Total += 1;
+      } else {
+        billingMapping[monthID] = { name: monthID, sortIndex: year + "" + (month < 10 ? "0" + month : month), Total: 1 };
+      }
+    }
+  });
 
   return (
     <div className="chart">
-      {/* <div className="title">Impact </div> */}
-      <ResponsiveContainer width="98%" aspect={2 / 0.9}>
+      <ResponsiveContainer width="98%" aspect={3}>
         <AreaChart
           width={500}
           height={180}
-          data={fakeData}
-          margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
+          data={Object.values(billingMapping).sort((a, b) => a.sortIndex - b.sortIndex)}
+          margin={{ top: 0, right: 0, left: -20, bottom: 0 }}
         >
           <defs>
             <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
@@ -37,14 +51,15 @@ export const Chart = ({ data }) => {
               <stop offset="95%" stopColor="#8A9DF8" stopOpacity={0} />
             </linearGradient>
           </defs>
-          <XAxis dataKey="name" stroke="gray" />
+          <XAxis dataKey="name" stroke="gray" dy={5} />
           <YAxis domain={[0, "dataMax +1"]} />
           <CartesianGrid strokeDasharray="3 3" className="chartGrid" />
           <Tooltip />
           <Area
             type="monotone"
             dataKey="Total"
-            stroke="#8884d8"
+            stroke="#5974E9"
+            strokeWidth={4}
             fillOpacity={1}
             fill="url(#total)"
           />
