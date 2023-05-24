ddae548aaa5139ff0d673336e09f32246a5e9b06
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-05-24 03:43:03 -0700

    make subtask active ticket when clicked on parent

--- frontend/src/components/TicketBoard/Subtasks/index.js
+++ frontend/src/components/TicketBoard/Subtasks/index.js
@@ -1,41 +1,49 @@
 import { useDispatch, useSelector } from "react-redux";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
-import { Checkbox } from "@mui/material";
 import "./index.css";
 import Add from "../../../assets/AddBlack.png";
-import { ADD_SUBTASKS } from "../../../redux/actions/ticketActions";
+import {
+  ADD_SUBTASKS,
+  SET_ACTIVE_TICKET
+} from "../../../redux/actions/ticketActions";
 
 function Subtasks({ readOnly }) {
   const dispatch = useDispatch();
+  const ticketBoardData = useSelector(
+    (state) => state.ticketReducer.ticketBoardDndData
+  );
   const currentTicket = useSelector(
     (state) => state.ticketReducer.currentTicket
   );
   const currentTicketSubtasks = useSelector(
     (state) => state.ticketReducer.currentTicketSubtasks
   );
+  const allTasks = ticketBoardData.tasks;
   return (
-    <div className="ticketSubtasks" style={{ borderRadius: readOnly ? 10 : null }}>
+    <div
+      className="ticketSubtasks"
+      style={{ borderRadius: readOnly ? 10 : null }}
+    >
       <div className="ticketSubtasksContainer">
         <div className="contentList">
           <div className="ticketSectionTitle">Subtasks</div>
           <div className="subtaskCostRows">
             {currentTicketSubtasks.map((subtasks) => {
               return (
-                <div className="subtaskCostRow" key={subtasks.subtask_id}>
+                <div
+                  className="subtaskCostRow"
+                  key={subtasks.subtask_id}
+                  onClick={() => {
+                    const subtaskTaskData = allTasks[subtasks?.subtask_uuid];
+                    dispatch({
+                      type: SET_ACTIVE_TICKET,
+                      payload: subtaskTaskData,
+                    });
+                  }}
+                >
                   <div className="subtaskInputContainer">
-                    <FormControl style={{ width: "100%" }}>
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            onClick={() =>
-                              console.log("TODO: Mark subtask as complete")
-                            }
-                          />
-                        }
-                        label={<span className="subtasksLabel">{subtasks.subtask_title}</span>}
-                      />
-                    </FormControl>
+                    <span className="subtasksLabel">
+                      {subtasks.subtask_title}
+                    </span>
                   </div>
                 </div>
               );
@@ -43,18 +51,20 @@ function Subtasks({ readOnly }) {
           </div>
         </div>
 
-        {!readOnly ? <div
-          className="additionBar"
-          onClick={() => {
-            dispatch({
-              type: ADD_SUBTASKS,
-              payload: { task_id: currentTicket.code },
-            });
-          }}
-        >
-          <img className="Add" src={Add} alt="Add" />
-          <div className="ticketSectionTitle">Add Subtask</div>
-        </div> : null}
+        {!readOnly ? (
+          <div
+            className="additionBar"
+            onClick={() => {
+              dispatch({
+                type: ADD_SUBTASKS,
+                payload: { task_id: currentTicket.code },
+              });
+            }}
+          >
+            <img className="Add" src={Add} alt="Add" />
+            <div className="ticketSectionTitle">Add Subtask</div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
